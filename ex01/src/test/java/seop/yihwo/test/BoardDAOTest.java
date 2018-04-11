98a65071f003c797be18434152209fe3cb003288
every <every@DESKTOP-KRH3CPG>
2018-04-12 00:15:18 +0900

    게시판 예제 DAO테스트 오류

---
+++ ex01/src/test/java/seop/yihwo/test/BoardDAOTest.java
@@ -0,0 +1,33 @@
+package seop.yihwo.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import seop.yihwo.domain.*;
+import seop.yihwo.persistence.BoardDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class BoardDAOTest {
+	
+	@Inject
+	private BoardDAO dao;
+	
+	private static Logger logger = LoggerFactory.getLogger(BoardDAOTest.class);
+	
+	@Test
+	public void testCreate() throws Exception {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로운 글을 넣습니다. ");
+		board.setContent("새로운 글을 넣습니다. ");
+		board.setWriter("user00");
+		dao.create(board);
+	}
+	
+}
